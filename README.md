d78a497a2dcc6d949e814ea3edbd6346c6f94af9
Mohamedizzath <mohamedizzath2016@gmail.com>
2022-08-27 13:17:13 +0530

    Answers for the Activity-02

---
+++ README.md
@@ -0,0 +1,23 @@
+# Express app which implements basic authentication and file upload
+
+---
+### Third party frameworks which used in this project
+1. [Ejs - Template engine](https://ejs.co/)
+2. [Express - Web framework for Node.js](https://expressjs.com/)
+3. [Express sessions - Used for creating sessions ](https://www.npmjs.com/package/express-session)
+4. [Multer - File uploading library](https://www.npmjs.com/package/multer)
+5. [Passport js - Authentication for Node.js](https://www.passportjs.org/)
+6. [Nodemon - For easy development process](https://www.npmjs.com/package/nodemon)
+
+---
+### How to run the project
+
+1. Install dependencies 
+    `npm install`
+2. Running the project
+    `npm start`
+
+---
+### How the project should look when running
+
+![Screenshot](./src/images/Screenshot.png)
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 const path = require('path');
 
 const app = express();
@@ -17,12 +18,25 @@ app.use(passport.session());
 // Using cookie parser to work with cookies
 app.use(cookieParser());
 
-// Current valid users
-const users = [{id: 1, email: 'first@example.com', password: '123'}, {id: 2, email: 'second@example.com', password: '123'}, {id: 3, email: 'third@example.com', password: '123'}];
-
 // Using ejs for the template engine
 app.set('view engine', 'ejs');
 app.set('views', 'src');
+
+// Setting up multer
+const storage = multer.diskStorage({
+    destination: './src/file-uploads/',
+    filename: (req, file, callback) => {
+        callback(null, req.user.email + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 10485760} //10485760 // 10MB
+}).single('fileUpload');
+
+// Current valid users
+const users = [{id: 1, email: 'first@example.com', password: '123'}, {id: 2, email: 'second@example.com', password: '123'}, {id: 3, email: 'third@example.com', password: '123'}];
 
 const initializePassport = require('./passport');
 initializePassport(passport, email => users.find(user => user.email === email),id => users.find(user => user.id === id));
@@ -43,13 +57,38 @@ app.get('/', (req, res)=>{
 
 app.get('/login', (req, res) => {
     // Returning the login.ejs web page
-    res.render('login', );
+    res.render('login');
 });
 
 app.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
 }));
+
+app.get('/uploads', (req, res) => {
+    // Check the user is authenticated
+    if(req.user){
+        // Given user is authenticated
+        res.render('uploads');
+    }else{
+        // Given user is not authenticated
+        res.redirect('/login');
+    }
+
+    // TODO: For the development - needs to remove
+    // res.render('uploads');
+});
+
+app.post('/uploads', (req, res) => {
+    upload(req, res, (err) => {
+       // Check that there exists an error
+       if(err){
+           res.render('uploads', { message: err, status: 'error' });
+       }else{
+           res.render('uploads', { message: 'Upload completed', status: 'success'});
+       }
+    });
+})
 
 app.listen(port, ()=>{
     console.log('Running on the port 8080');
